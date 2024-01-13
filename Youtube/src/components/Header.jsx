3403d8684d7705a268df596b7a2cac65042bbdca
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 01:06:04 +0530

    logo ready

--- Youtube/src/components/Header.jsx
+++ Youtube/src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import mainLogo from "../assets/yt-logo-main.png"
+import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
     return (
         <div>
-            <div>
-                <img src={mainLogo} alt="" width={110} />
+            <div className='flex gap-2 items-center pl-3 pt-2'>
+                <GiHamburgerMenu className='text-3xl ' />
+                <img src={mainLogo} alt="" width={100} className='h-6' />
             </div>
+            <div>
+                
+            </div>
+            <div></div>
         </div>
     )
 }

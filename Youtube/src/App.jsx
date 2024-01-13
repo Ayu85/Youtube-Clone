5cddb429ade19dee15cd3f3214c23e51dc3d60c9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 00:52:22 +0530

    youtube clone started

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
+import Body from './components/Body'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Body />
     </>
   )
 }
---
+++ Youtube/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import mainLogo from "../assets/yt-logo-main.png"
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src={mainLogo} alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Header

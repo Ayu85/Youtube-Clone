6a08a54ce2cce600d2b2e608cbbb8a424482232a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 01:31:08 +0530

    header fully responsive

--- Youtube/src/components/Header.jsx
+++ Youtube/src/components/Header.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import mainLogo from "../assets/yt-logo-main.png"
+import { IoIosSearch } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaUserCircle } from "react-icons/fa";
 const Header = () => {
     return (
-        <div>
-            <div className='flex gap-2 items-center pl-3 pt-2'>
+        <div className='flex   justify-between py-4 px-3'>
+            <div className='flex gap-2 items-center  '>
                 <GiHamburgerMenu className='text-3xl ' />
-                <img src={mainLogo} alt="" width={100} className='h-6' />
+                <img src={mainLogo} alt=""  className='' width={110} />
             </div>
-            <div>
-                
+            <div className='flex '>
+                <input type="text" placeholder='Search' className='border-slate-300 border pl-3  py-2 max-md:w-[200px] max-sm:w-[100px]  w-[500px] rounded-l-full' name="" id="" />
+                <button className='py-2 font-bold px-4 rounded-r-full border text-xl border-l-white border-slate-300'><IoIosSearch /></button>
             </div>
-            <div></div>
+            <div className='text-3xl'>
+                <FaUserCircle />
+            </div>
         </div>
     )
 }

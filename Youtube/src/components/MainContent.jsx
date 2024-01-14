61a1c8f057f8fd491c9176a0f0a933ebb2356f85
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 14:03:00 +0530

    buttons modified

--- Youtube/src/components/MainContent.jsx
+++ Youtube/src/components/MainContent.jsx
@@ -7,12 +7,12 @@ const MainContent = () => {
         <div className=' w-[1400px]' style={!isSideOpen ? { translate: "-30px", transition: "all .3s ease-in-out ", width: "6000px" } : { transition: "all .3s ease-in-out "}}>
             <div className='font-semibold'>
                 {/* button list */}
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>All</button>
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>Shorts</button>
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>Videos</button>
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>Unwatched</button>
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>Watched</button>
-                <button className='m-2 border-slate-300 border px-4 py-1 rounded-lg'>Recently Uploaded</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>All</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>Shorts</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>Videos</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>Unwatched</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>Watched</button>
+                <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>Recently Uploaded</button>
             </div>
             <div>
                 {/* video container */}

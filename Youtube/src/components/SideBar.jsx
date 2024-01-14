9b3e8c6e5fc31a9150846e535e3f5ce3114b9025
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 23:29:40 +0530

    channel logo added

--- Youtube/src/components/SideBar.jsx
+++ Youtube/src/components/SideBar.jsx
@@ -16,7 +16,7 @@ import SideBarSymbolOnly from './sideBarSymbolOnly';
 const SideBar = () => {
     const isClicked = useSelector(store => store.slice.value)
     console.log(isClicked);
-    return !isClicked ? <SideBarSymbolOnly /> : (
+    return !isClicked ? <SideBarSymbolOnly  /> : (
         <div className='w-72 pt-3  overflow-y-scroll h-[100vh] scroll-smooth  ' style={!isClicked ? { translate: "-265px", transition: ".3s all ease-in-out" } : { transition: ".3s all ease-in-out" }}>
             <div className='list-none  font-semibold pl-3 tracking-wide text-lg border-b border-slate-300'>
                 <li className='flex mb-3 items-center gap-3'><IoIosHome className='text-3xl text-zinc-700' />Home</li>

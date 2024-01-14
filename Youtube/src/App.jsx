6aa5af570d441c89a0cb47a5af63c7491311f858
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 09:21:28 +0530

    code optimized

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Body from './components/Body'
+import Header from './components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <Body />
     </>
   )
--- Youtube/src/components/Body.jsx
+++ Youtube/src/components/Body.jsx
@@ -3,7 +3,7 @@ import Header from './Header'
 const Body = () => {
   return (
     <div>
-      <Header/>
+        {/* sidebar and maincontainer */}
     </div>
   )
 }

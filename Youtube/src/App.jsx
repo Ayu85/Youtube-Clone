fc11794f631b623fe15a38b6920a5e41b4f57b59
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-15 00:34:28 +0530

    router ready

--- Youtube/src/App.jsx
+++ Youtube/src/App.jsx
@@ -3,12 +3,26 @@ import Body from './components/Body'
 import Header from './components/Header'
 import store from './utils/store'
 import { Provider } from 'react-redux'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import WatchPage from './components/WatchPage'
+const appRoute = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />,
+
+  },
+  {
+    path: '/watch',
+    element: <WatchPage />
+  }
+])
 function App() {
 
   return (
     <Provider store={store}>
       <Header />
-      <Body />
+      <RouterProvider router={appRoute} />
+      <Outlet />
     </Provider>
   )
 }
---
+++ Youtube/src/components/WatchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WatchPage = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti saepe magni, numquam voluptates id deleniti enim illo iusto amet! Vero consectetur id, quae saepe ex velit harum minus sit exercitationem similique corporis laborum iure voluptates rerum doloribus quaerat debitis qui cupiditate omnis, perspiciatis, obcaecati illo quas inventore ipsam. Dolor, saepe.</h1>
+    </div>
+  )
+}
+
+export default WatchPage

336aec45eb67ac1e8182fd5538c0666b44d083e0
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 14:38:40 +0530

    youtube api ready

--- Youtube/src/components/MainContent.jsx
+++ Youtube/src/components/MainContent.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { VIDEO_API } from '../utils/constants'
 
 const MainContent = () => {
     const isSideOpen = useSelector(store => store.slice.value)
+    const [videos, setVideos] = useState([])
+    useEffect(() => {
+        const getVideoData = async () => {
+            const raw = await fetch(VIDEO_API);
+            const json = await raw.json();
+            setVideos(json?.items)
+        }
+        getVideoData()
+    }, [])
+    console.log(videos);
     return (
-        <div className=' w-[1400px]' style={!isSideOpen ? { translate: "-30px", transition: "all .3s ease-in-out ", width: "6000px" } : { transition: "all .3s ease-in-out "}}>
+        <div className=' w-[1400px]' style={!isSideOpen ? { translate: "-30px", transition: "all .3s ease-in-out ", width: "6000px" } : { transition: "all .3s ease-in-out " }}>
             <div className='font-semibold'>
                 {/* button list */}
                 <button className='m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg'>All</button>
@@ -16,6 +27,7 @@ const MainContent = () => {
             </div>
             <div>
                 {/* video container */}
+
             </div>
         </div>
     )
---
+++ Youtube/src/utils/constants.jsx
@@ -0,0 +1,2 @@
+const API_KEY = "AIzaSyDg-s0MYk0trSakXz7e4Lzm3Xgilj7-7Ws"
+export const VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${API_KEY}`

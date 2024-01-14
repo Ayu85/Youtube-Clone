5b4cf9076cf75c4cbc94ebaa1ad917b4d139d01f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 13:28:31 +0530

    side bar scrollable

--- Youtube/src/components/SideBar.jsx
+++ Youtube/src/components/SideBar.jsx
@@ -16,7 +16,7 @@ const SideBar = () => {
     const isClicked = useSelector(store => store.slice.value)
     console.log(isClicked);
     return (
-        <div className='w-72 pt-3 hidden lg:block  ' style={!isClicked ? { translate: "-265px", transition: ".3s all ease-in-out" } : { transition: ".3s all ease-in-out" }}>
+        <div className='w-72 pt-3 hidden lg:block overflow-y-scroll h-[100vh] scroll-smooth  ' style={!isClicked ? { translate: "-265px", transition: ".3s all ease-in-out" } : { transition: ".3s all ease-in-out" }}>
             <div className='list-none  font-semibold pl-3 tracking-wide text-lg border-b border-slate-300'>
                 <li className='flex mb-3 items-center gap-3'><IoIosHome className='text-3xl text-zinc-700' />Home</li>
                 <li className='flex mb-3 items-center gap-3'> <SiYoutubeshorts className='text-3xl text-zinc-700' />Shorts</li>
@@ -27,6 +27,27 @@ const SideBar = () => {
                 <li className='flex mb-3 items-center gap-3'> <FaHistory className='text-2xl text-zinc-700' />History</li>
             </div>
             <div className='list-none font-semibold pl-3 tracking-wide text-lg mt-6 border-b border-slate-300'>
+                <h1 className='mb-2 font-bold text-xl'>Explore</h1>
+                <li className='flex mb-3 items-center gap-3'><FaFire className='text-2xl text-zinc-700' />Trending</li>
+                <li className='flex mb-3 items-center gap-3'><FaBagShopping className='text-2xl text-zinc-700' />Shopping</li>
+                <li className='flex mb-3 items-center gap-3'><SiYoutubemusic className='text-2xl text-zinc-700' />Music</li>
+                <li className='flex mb-3 items-center gap-3'><RiMovie2Fill className='text-2xl text-zinc-700' />Movies</li>
+                <li className='flex mb-3 items-center gap-3'><IoGameController className='text-2xl text-zinc-700' />Gaming</li>
+                <li className='flex mb-3 items-center gap-3'><MdSportsCricket className='text-2xl text-zinc-700' />Sports</li>
+                <li className='flex mb-3 items-center gap-3'><SiStudyverse className='text-2xl text-zinc-700' />Learning</li>
+
+            </div>
+            <div className='list-none font-semibold pl-3 tracking-wide text-lg mt-6 border-b border-slate-300'>
+                <h1 className='mb-2 font-bold text-xl'>Explore</h1>
+                <li className='flex mb-3 items-center gap-3'><FaFire className='text-2xl text-zinc-700' />Trending</li>
+                <li className='flex mb-3 items-center gap-3'><FaBagShopping className='text-2xl text-zinc-700' />Shopping</li>
+                <li className='flex mb-3 items-center gap-3'><SiYoutubemusic className='text-2xl text-zinc-700' />Music</li>
+                <li className='flex mb-3 items-center gap-3'><RiMovie2Fill className='text-2xl text-zinc-700' />Movies</li>
+                <li className='flex mb-3 items-center gap-3'><IoGameController className='text-2xl text-zinc-700' />Gaming</li>
+                <li className='flex mb-3 items-center gap-3'><MdSportsCricket className='text-2xl text-zinc-700' />Sports</li>
+                <li className='flex mb-3 items-center gap-3'><SiStudyverse className='text-2xl text-zinc-700' />Learning</li>
+
+            </div>   <div className='list-none font-semibold pl-3 tracking-wide text-lg mt-6 border-b border-slate-300'>
                 <h1 className='mb-2 font-bold text-xl'>Explore</h1>
                 <li className='flex mb-3 items-center gap-3'><FaFire className='text-2xl text-zinc-700' />Trending</li>
                 <li className='flex mb-3 items-center gap-3'><FaBagShopping className='text-2xl text-zinc-700' />Shopping</li>

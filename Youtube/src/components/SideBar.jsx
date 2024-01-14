cabb9c2747947affb717232957c56b5d3a417031
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-14 09:28:06 +0530

    sidebar making

--- Youtube/src/components/SideBar.jsx
+++ Youtube/src/components/SideBar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const SideBar = () => {
     return (
         <div>
-            <div></div>
+            <div>
+                <li>Home</li>
+                <li>Shorts</li>
+                <li>Subscriptions</li>
+            </div>
             <div></div>
         </div>
     )

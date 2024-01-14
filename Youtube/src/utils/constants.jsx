40df7a105ab7336b4281d4a45b14208ff84cefde
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-15 00:06:38 +0530

    react router installed

--- Youtube/src/utils/constants.jsx
+++ Youtube/src/utils/constants.jsx
@@ -1,2 +1,2 @@
 const API_KEY = "AIzaSyDg-s0MYk0trSakXz7e4Lzm3Xgilj7-7Ws"
-export const VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${API_KEY}`
+export const VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`

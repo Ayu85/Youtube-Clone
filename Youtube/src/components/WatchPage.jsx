b9d150d856ca202af26e8c9da95be6fbd52c6f85
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-15 14:15:40 +0530

    video player ready

--- Youtube/src/components/WatchPage.jsx
+++ Youtube/src/components/WatchPage.jsx
@@ -22,18 +22,21 @@ const WatchPage = () => {
   return (
     <div className='flex '>
       <SideBarSymbolOnly />
-      <div className='pt-8'>
-        <iframe width="853"
-          height="450" src={"https://www.youtube.com/embed/" + videoId}
+      <div className='pt-8 '>
+        <iframe width={400} height={300} style={{ borderRadius: "25px" }}
+          src={"https://www.youtube.com/embed/" + videoId}
           title="Video Player"
           frameborder="0"
+          className='rounded-xl lg:h-[460px] lg:w-[900px]  md:w-[700px] md:h-[420px] p-3 sm:w-[500px] sm:h-[300px]'
           allow="accelerometer; autoplay; clipboard-write; encrypted-media
            ; gyroscope; picture-in-picture; web-share"
           allowfullscreen></iframe>
-        <h1 className='text-2xl p-1 font-semibold'>{currentVideoData?.snippet?.title}</h1>
-        <div className='flex justify-between items-center '>
-          <h2 className='flex items-center gap-1 text-xl font-bold'><FaCircleCheck />
-            {currentVideoData?.snippet?.channelTitle}</h2>
+        <h1 className='text-2xl p-1 font-semibold  lg:w-[900px]  md:w-[700px]   sm:w-[500px]'>{currentVideoData?.snippet?.title}</h1>
+        <div className='flex justify-between items-center  lg:w-[900px]  md:w-[700px]  p-3 sm:w-[500px] '>
+          <div className='flex flex-col'> <h2 className='flex items-center gap-1 text-xl font-bold'><FaCircleCheck />
+            {currentVideoData?.snippet?.channelTitle}
+          </h2>
+            <h2 className='font-semibold pl-6'>{currentVideoData?.statistics?.viewCount} Views</h2></div>
           <div className='flex gap-4'>
             <button className='flex items-center gap-1 border-slate-300 border rounded-full text-xl font-semibold px-5 py-2'>
               <AiTwotoneLike className='text-2xl' />

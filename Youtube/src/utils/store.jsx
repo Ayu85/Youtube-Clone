fad53ac99e35f9ada67e61e83139d4862c62daa5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 15:36:55 +0530

    slice provided to store

--- Youtube/src/utils/store.jsx
+++ Youtube/src/utils/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import HamburgerSlice from "./slices/HamburgerSlice";
+import CommentSlice from "./slices/CommentSlice";
 const store = configureStore({
     reducer: {
-        slice: HamburgerSlice
+        slice: HamburgerSlice,
+        comment: CommentSlice
     }
 })
 

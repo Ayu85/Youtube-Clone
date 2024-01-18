f2e9e23bfc8b7f157d429545e038a3ad5a785ec5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 21:35:47 +0530

    live chat working

--- Youtube/src/components/LiveChat.jsx
+++ Youtube/src/components/LiveChat.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { FcOnlineSupport } from "react-icons/fc";
 import { useDispatch, useSelector } from 'react-redux';
 import { addComment, removeComment } from '../utils/slices/CommentSlice';
+import useName from '../utils/useName';
 const Comments = ({ name, text, logo }) => {
     return <div className='cmnt flex gap-2 items-center pt-3 shadow-sm pb-1'>
         <img src={logo} alt="" width={20} />
@@ -13,18 +14,20 @@ const LiveChat = () => {
     const sliceComments = useSelector(store => store.comment.data)
 
     const dispatch = useDispatch();
+
     useEffect(() => {
         setInterval(() => {
+            const username = useName();
             dispatch(addComment({
-                name: "Akshay Saini",
-                text: "This is akshay from UBER",
+                name: username,
+                text: `This is ${username} from UBER`,
                 logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm9t4ColDnY4w2KJuyo3VQk9l41rWpG35ullBgK_N31C9lKwMVdS9qm90BkeLUKStT4oY & usqp=CAU"
             }))
         }, 3000);
-      
-         setInterval(() => {
-             dispatch(removeComment())
-         }, 80000)
+
+        setInterval(() => {
+            dispatch(removeComment())
+        }, 80000)
 
     }, [])
     return (
@@ -34,7 +37,7 @@ const LiveChat = () => {
                     return <Comments   {...items} />
                 })
             }
-            
+
         </div>
     )
 }

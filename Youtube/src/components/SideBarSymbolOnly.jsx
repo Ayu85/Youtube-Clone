ed2db1858e5dd61c4dc0bc79cab0590e64530666
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 12:52:33 +0530

    live icon added

--- Youtube/src/components/SideBarSymbolOnly.jsx
+++ Youtube/src/components/SideBarSymbolOnly.jsx
@@ -13,12 +13,15 @@ import { RiMovie2Fill } from "react-icons/ri";
 import { IoGameController } from "react-icons/io5";
 import { SiStudyverse } from "react-icons/si";
 import { Link } from 'react-router-dom';
+import { RiLiveFill } from "react-icons/ri";
+
 const SideBarSymbolOnly = () => {
     return (
         <div className='flex flex-col w-48 gap-3  h-auto pl-2 pt-3 transition-all '>
             <Link to={'/'}><IoIosHome className='text-5xl text-zinc-700 cursor-pointer border-b border-slate-300 pb-3 ' /></Link>
             <SiYoutubeshorts className='text-5xl text-zinc-700 cursor-pointer border-b border-slate-300 pb-3' />
             <MdSubscriptions className='text-5xl text-zinc-700 cursor-pointer border-b border-slate-300 pb-3' />
+            <RiLiveFill className=' text-[#FF0000] text-5xl cursor-pointer border-b border-slate-300 pb-3' />
             <MdFeaturedPlayList className='text-5xl text-zinc-7 cursor-pointer border-b border-slate-300 pb-3' />
             <FaHistory className='text-5xl text-zinc-700 cursor-pointer border-b border-slate-300 pb-3' />
             <FaFire className='text-5xl text-zinc-700 cursor-pointer border-b border-slate-300 pb-3' />

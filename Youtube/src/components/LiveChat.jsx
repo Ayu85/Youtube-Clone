047c9ea16f47ea95aac8d10295d80f58a57b6837
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 15:26:19 +0530

    comments ready to render

--- Youtube/src/components/LiveChat.jsx
+++ Youtube/src/components/LiveChat.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { FcOnlineSupport } from "react-icons/fc";
 
+const Comments = () => {
+    return <div className='flex gap-2 items-center pt-3 shadow-sm pb-1'>
+        <FcOnlineSupport className='text-2xl' />
+        <div className='font-bold text-lg'>Aayush Singh</div>
+        <div className='pl-2 pt-[2px]'>This is aayush SDE 1 at AMAZON</div>
+    </div>
+}
 const LiveChat = () => {
     return (
-        <div >
+        <div>
+            <Comments />
+            <Comments />
+            <Comments />
+            <Comments />
+            <Comments />
+            <Comments />
+            <Comments />
+
         </div>
     )
 }

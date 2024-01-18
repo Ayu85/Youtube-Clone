0a7a894db6074a6635b04a3c8b2d0e9a0bf64348
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 15:04:38 +0530

    live coment page

---
+++ Youtube/src/components/LiveChat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LiveChat = () => {
+    return (
+        <div className='bg-slate-100'>
+            <h1>COMMENTS :</h1>
+        </div>
+    )
+}
+
+export default LiveChat

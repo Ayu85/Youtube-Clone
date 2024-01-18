82ffbe9bbe4bda2c3ab13b8fd8125d354d1d75ac
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 15:46:49 +0530

    comment slice working

--- Youtube/src/components/LiveChat.jsx
+++ Youtube/src/components/LiveChat.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { FcOnlineSupport } from "react-icons/fc";
 import { useDispatch, useSelector } from 'react-redux';
 import { addComment } from '../utils/slices/CommentSlice';
-const Comments = () => {
+const Comments = ({ name, text }) => {
     const sliceComments = useSelector(store => store.comment.data)
     return <div className='flex gap-2 items-center pt-3 shadow-sm pb-1'>
         <FcOnlineSupport className='text-2xl' />
-        <div className='font-bold text-lg'>{sliceComments[0].name}</div>
-        <div className='pl-2 pt-[2px]'>{sliceComments[0].text}</div>
+        <div className='font-bold text-lg'>{name}</div>
+        <div className='pl-2 pt-[2px]'>{text}</div>
     </div>
 }
 const LiveChat = () => {
@@ -15,13 +15,13 @@ const LiveChat = () => {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(addComment({
-            name: "Akshay",
+            name: "Akshay Saini",
             text: "This is akshay from UBER"
         }))
     }, [])
     return (
         <div>
-            <Comments />
+            <Comments   {...sliceComments[0]} />
         </div>
     )
 }

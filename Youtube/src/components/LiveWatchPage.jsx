5adbabfde09a47286d3333af30ef2d22aba6e661
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-18 14:24:14 +0530

    live videos page ready

--- Youtube/src/components/LiveWatchPage.jsx
+++ Youtube/src/components/LiveWatchPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import SideBarSymbolOnly from './SideBarSymbolOnly'
+import VideoPlayer from './VideoPlayer'
+import { useSearchParams } from 'react-router-dom'
+import { API_KEY } from '../utils/constants'
 const LiveWatchPage = () => {
+    const [currentVideoData, setData] = useState([])
+    const [rawvideoId] = useSearchParams();
+    const videoId = rawvideoId.get("v")
+    useEffect(() => {
+        const getVideoData = async () => {
+            const raw = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${API_KEY}`)
+            const json = await raw.json();
+            console.log(json);
+            setData(json?.items[0])
+        }
+        // getVideoData();
+    }, [])
     return (
-        <div>
+        <div className='flex '>
             <SideBarSymbolOnly />
+            <div className='pt-5'>
+                <VideoPlayer videoId={videoId} currentVideoData={currentVideoData} />
+            </div>
             
         </div>
     )

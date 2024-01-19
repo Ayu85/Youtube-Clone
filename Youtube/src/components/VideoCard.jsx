4c88e2021a2f2d6d50ed551798dc4eadceb13d5b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 00:32:06 +0530

    video cards dark mode enabled

--- Youtube/src/components/VideoCard.jsx
+++ Youtube/src/components/VideoCard.jsx
@@ -3,21 +3,24 @@ import { ISO8601toDuration } from '../utils/useTime';
 import { FcOk } from "react-icons/fc"
 import { FaUserCircle } from "react-icons/fa";
 import { MdOnlinePrediction } from "react-icons/md";
+import { useSelector } from 'react-redux';
 const VideoCard = ({ snippet, statistics, contentDetails }) => {
     const duration = ISO8601toDuration(contentDetails?.duration);
+    const theme = useSelector(store => store.theme.isDark)
     return (
-        <div className='relative w-[375px] h-[330px] py-1   shadow-lg shadow-slate-200 '>
+        <div className={`relative w-[375px] h-[330px] py-1   shadow-lg shadow-slate-200 ${theme && "text-slate-50 shadow-[#2c2c2c] "}`}>
             <div className='relative'>
                 <img src={snippet?.thumbnails?.maxres?.url} width={375} className='absolute rounded-xl' alt="" />
                 <h4 className='absolute bg-[#000000ad] text-sm text-white px-2 py-1 rounded-lg top-48 right-2'>{duration || ""}</h4>
             </div>
-            <h1 className='w-[330px] h-14 overflow-hidden font-semibold text-lg text-slate-700 ml-3 mt-[215px]'>{snippet.title}</h1>
+            <h1 className={`w-[330px] h-14 overflow-hidden font-semibold text-lg text-slate-700 ml-3 mt-[215px]  ${theme && "text-slate-50"}`}>{snippet.title}</h1>
             <h2 className='flex items-center gap-2 text-lg font-bold  ml-3'> <FaUserCircle className='text-[#FF0000] text-2xl' />{snippet.channelTitle}</h2>
             <h3 className=' ml-3'>{statistics.viewCount} Views</h3>
 
         </div>
     )
 }
+// higher order functions
 export const LiveVideoCard = ({ snippet, statistics, contentDetails }) => {
 
     return (

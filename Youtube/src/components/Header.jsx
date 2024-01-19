7074e6fd7f85f078a1982ffd195bb65766f6681d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-19 18:56:37 +0530

    theme toggler

--- Youtube/src/components/Header.jsx
+++ Youtube/src/components/Header.jsx
@@ -63,8 +63,17 @@ const Header = () => {
                 </div>
             </div>
 
-            <div className='text-3xl'>
-                <FaUserCircle />
+            <div >
+
+            </div>
+            {/* theme toogler */}
+            <div className=' flex gap-3 items-center pr-2'>
+                <FaUserCircle className='text-3xl' />
+                <label className="relative flex items-center cursor-pointer">
+                    <input type="checkbox" value="" className="sr-only peer" />
+                    <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[3px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span className="ms-3 text-lg font-medium text-gray-900 dark:text-gray-700">Dark Mode</span>
+                </label>
             </div>
         </div>
     )

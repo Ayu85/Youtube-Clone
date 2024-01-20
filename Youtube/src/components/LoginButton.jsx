2257cc767875f9f280a13d0f6a23f3229e3608d9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 20:18:53 +0530

    login feature added

--- Youtube/src/components/LoginButton.jsx
+++ Youtube/src/components/LoginButton.jsx
@@ -6,8 +6,10 @@ import { FaUserCircle } from "react-icons/fa";
 const LoginButton = () => {
     const theme = useSelector(store => store.theme.isDark)
     const { loginWithRedirect } = useAuth0();
-    return <FaUserCircle onClick={() => loginWithRedirect()}
-        className={`text-3xl cursor-pointer  ${theme && "text-white"} ${!theme && "text-black"}`} />
+    return <div className={`flex px-2 gap-2 rounded-full items-center ${theme && "border-[#929292] border"} ${!theme && "border-slate-400 border"}`}>
+        <FaUserCircle className={`text-2xl  ${theme && "text-white"} `} />
+        <button onClick={() => loginWithRedirect()} className={`  py-1 
+     ${theme && "text-blue-400 "}`}>Login</button></div>
 };
 
 export default LoginButton;

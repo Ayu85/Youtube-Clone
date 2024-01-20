14d59e312fa335713e6a39eee5178e71f1cbb4d4
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 18:13:43 +0530

    video card refactored

--- Youtube/src/components/VideoCard.jsx
+++ Youtube/src/components/VideoCard.jsx
@@ -13,8 +13,8 @@ const VideoCard = ({ snippet, statistics, contentDetails }) => {
                 <img src={snippet?.thumbnails?.maxres?.url} width={375} className='absolute rounded-xl' alt="" />
                 <h4 className='absolute bg-[#000000ad] text-sm text-white px-2 py-1 rounded-lg top-48 right-2'>{duration || ""}</h4>
             </div>
-            <h1 className={`w-[330px] h-14 overflow-hidden font-semibold text-lg text-slate-700 ml-3 mt-[215px]  ${theme && "text-slate-50"}`}>{snippet.title}</h1>
-            <h2 className='flex items-center gap-2 text-lg font-bold  ml-3'> <FaUserCircle className='text-[#FF0000] text-2xl' />{snippet.channelTitle}</h2>
+            <h1 className={`w-[330px] h-14 overflow-hidden font-semibold text-lg text-slate-700 ml-3 mt-[215px]  ${theme && "text-slate-300"}`}>{snippet.title}</h1>
+            <h2 className={`flex items-center gap-2 text-lg font-bold  ml-3 ${theme&& "text-slate-300"}`}> <FaUserCircle className='text-[#FF0000] text-2xl' />{snippet.channelTitle}</h2>
             <h3 className=' ml-3'>{statistics.viewCount} Views</h3>
 
         </div>

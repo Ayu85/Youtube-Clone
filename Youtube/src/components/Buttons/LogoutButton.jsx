28c3015f1467c1fe66e526f3126fd859e7224d8e
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 20:29:14 +0530

    login and logout feature added

--- Youtube/src/components/Buttons/LogoutButton.jsx
+++ Youtube/src/components/Buttons/LogoutButton.jsx
@@ -1,13 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { useSelector } from "react-redux";
+import { FaUserCircle } from "react-icons/fa";
 
 const LogoutButton = () => {
     const { logout } = useAuth0();
-
+    const theme = useSelector(store => store.theme.isDark)
     return (
-        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-            Log Out
-        </button>
+        <div className={`flex px-2 gap-2 rounded-full items-center ${theme && "border-[#929292] border"} ${!theme && "border-slate-400 border"}`}>
+            <FaUserCircle className={`text-2xl  ${theme && "text-white"} `} />
+            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} className={`  py-1 
+     ${theme && "text-blue-400 "}`}>Logout</button></div>
     );
 };
 

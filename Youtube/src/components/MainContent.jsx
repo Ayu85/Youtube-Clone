14dbfdc709a1e4d4e137f7b88e073e7077dd37c2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 18:21:51 +0530

    code refactored

--- Youtube/src/components/MainContent.jsx
+++ Youtube/src/components/MainContent.jsx
@@ -22,12 +22,12 @@ const MainContent = () => {
         <div className=' w-[100%]' >
             <div className={`font-semibold ${!isSideOpen && "pl-28"}`}>
                 {/* button list */}
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>All</button>
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>Shorts</button>
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>Videos</button>
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>Unwatched</button>
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>Watched</button>
-                <button className={`m-2 bg-slate-200 border-slate-300 border px-4 py-1 rounded-lg ${theme && "bg-[#272727] text-white border-none"}`}>Recently Uploaded</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg ${!theme && "bg-slate-300"} ${theme && "bg-[#272727] text-white border-none"}`}>All</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg  ${!theme && "bg-slate-300"}  ${theme && "bg-[#272727] text-white border-none"}`}>Shorts</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg  ${!theme && "bg-slate-300"} ${theme && "bg-[#272727] text-white border-none"}`}>Videos</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg  ${!theme && "bg-slate-300"} ${theme && "bg-[#272727] text-white border-none"}`}>Unwatched</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg  ${!theme && "bg-slate-300"} ${theme && "bg-[#272727] text-white border-none"}`}>Watched</button>
+                <button className={`m-2  border-slate-300 border px-4 py-1 rounded-lg  ${!theme && "bg-slate-300"} ${theme && "bg-[#272727] text-white border-none"}`}>Recently Uploaded</button>
             </div>
             <div className={`flex flex-wrap gap-6 items-center justify-start pt-5  pl-10 overflow-y-scroll h-auto ${!isSideOpen && "pl-28"}`}>
                 {/* video container */}

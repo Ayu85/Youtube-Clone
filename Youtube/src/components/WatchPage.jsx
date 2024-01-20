0c36d5f0819f1654065d3377993f56ecd8183f64
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 18:52:43 +0530

    watch page dark mode enabled

--- Youtube/src/components/WatchPage.jsx
+++ Youtube/src/components/WatchPage.jsx
@@ -11,12 +11,15 @@ import VideoDetailsSection_watchpage from './VideoDetailsSection_watchpage';
 import VideoPlayer from './VideoPlayer';
 // import { Link } from 'react-router-dom';
 import { allVideos } from '../utils/__mocks__'
+import { useSelector } from 'react-redux';
 
 const WatchPage = () => {
   const [SearchVideoId] = useSearchParams();
   const [currentVideoData, setData] = useState([])
   const [sideDisplayVideos, setDisplayVideos] = useState([])
   const [videos, setVideos] = useState([])
+  const theme = useSelector(store => store.theme.isDark)
+
   const videoId = SearchVideoId.get('v');
   useEffect(() => {
     const getVideoData = async () => {
@@ -38,34 +41,10 @@ const WatchPage = () => {
   }, [])
   //  console.log("display", sideDisplayVideos);
   return (
-    <div className='flex '>
+    <div className={`flex ${theme && "bg-[#0F0F0F] text-white"}`}>
       <SideBarSymbolOnly />
       <div className='pt-8 '>
         {/* VIDEO PLAYER AND NAME */}
-        {/* <div>  <iframe width={400} height={300} style={{ borderRadius: "25px" }}
-          src={"https://www.youtube.com/embed/" + videoId}
-          title="Video Player"
-          frameborder="0"
-          className='rounded-xl lg:h-[460px] lg:w-[800px]  md:w-[700px] md:h-[420px] p-3 sm:w-[500px] sm:h-[300px]'
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media
-           ; gyroscope; picture-in-picture; web-share"
-          allowfullscreen></iframe>
-          <h1 className='text-xl pl-4 font-semibold  lg:w-[800px]  md:w-[700px]   sm:w-[500px]'>{currentVideoData?.snippet?.title}</h1>
-          <div className='flex justify-between items-center  lg:w-[800px]  md:w-[700px]  p-3 sm:w-[500px] '>
-            <div className='flex flex-col '> <h2 className='flex items-center gap-1 text-xl font-bold'>
-              <FaUserCircle className='text-4xl' />{currentVideoData?.snippet?.channelTitle}
-            </h2>
-               <h2 className='font-semibold pl-6'>{currentVideoData?.statistics?.viewCount} Views</h2> 
-          </div>
-            <div className='flex gap-4'>
-              <button className='flex items-center gap-1 border-slate-300 border rounded-full text-xl font-semibold px-5 py-2'>
-                <AiTwotoneLike className='text-2xl' />
-                Like</button>
-              <button className='flex items-center gap-1 border-slate-300 border rounded-full text-xl font-semibold px-5 py-2'><PiShareFatDuotone className='text-2xl' />
-                Share</button>
-            </div>
-          </div>
-        </div> */}
         <div>
           <VideoPlayer videoId={videoId} currentVideoData={currentVideoData} />
         </div>

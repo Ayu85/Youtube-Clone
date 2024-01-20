07d95d259ef0a814f1392fb6f692e226cd42932f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 18:55:48 +0530

    side display videos dark mode enabled

--- Youtube/src/components/sideDisplayVideos_watchpage.jsx
+++ Youtube/src/components/sideDisplayVideos_watchpage.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { FaCircleCheck } from "react-icons/fa6";
+import { useSelector } from 'react-redux';
 const SideDisplayVideos_watchpage = ({ snippet, statistics, contentDetails }) => {
+    const theme = useSelector(store => store.theme.isDark)
     return (
         <div className='flex justify-between items-center h-28 '>
             <div>
-                <img src={snippet?.thumbnails?.standard?.url}   className=' h-28 w-52  md:rounded-none rounded-xl' alt="" />
+                <img src={snippet?.thumbnails?.standard?.url} className=' h-28 w-52  md:rounded-none rounded-xl' alt="" />
             </div>
-            <div ><h1 className='w-[200px] h-12 b line-clamp-2 overflow-hidden font-bold text-base text-slate-700 ml-3 '>{snippet?.title}</h1>
+            <div ><h1 className={`w-[200px] h-12 b line-clamp-2 overflow-hidden font-bold text-base text-slate-700 ml-3 ${theme && "text-slate-200"} `}>{snippet?.title}</h1>
                 <h2 className='flex items-center gap-1  font-semibold  ml-3'>{snippet?.channelTitle}<FaCircleCheck /></h2>
                 <h3 className=' ml-3'>{statistics?.viewCount} Views</h3></div>
-             </div>
+        </div>
     )
 }
 

0191740e49c7a6f6fd256e4cc46384b872959627
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-01-20 18:18:04 +0530

    dark mode refactored

--- Youtube/src/components/VideoCard.jsx
+++ Youtube/src/components/VideoCard.jsx
@@ -8,13 +8,13 @@ const VideoCard = ({ snippet, statistics, contentDetails }) => {
     const duration = ISO8601toDuration(contentDetails?.duration);
     const theme = useSelector(store => store.theme.isDark)
     return (
-        <div className={`relative w-[375px] h-[330px] py-1   shadow-lg shadow-slate-200 ${theme && "text-slate-50 shadow-[#2c2c2c9d]  "}`}>
+        <div className={`relative w-[375px] h-[330px] py-1    ${!theme && "shadow-lg shadow-slate-200"} ${theme && "text-slate-50 shadow-[#2c2c2c9d] shadow-lg  "}`}>
             <div className='relative'>
                 <img src={snippet?.thumbnails?.maxres?.url} width={375} className='absolute rounded-xl' alt="" />
                 <h4 className='absolute bg-[#000000ad] text-sm text-white px-2 py-1 rounded-lg top-48 right-2'>{duration || ""}</h4>
             </div>
-            <h1 className={`w-[330px] h-14 overflow-hidden font-semibold text-lg text-slate-700 ml-3 mt-[215px]  ${theme && "text-slate-300"}`}>{snippet.title}</h1>
-            <h2 className={`flex items-center gap-2 text-lg font-bold  ml-3 ${theme&& "text-slate-300"}`}> <FaUserCircle className='text-[#FF0000] text-2xl' />{snippet.channelTitle}</h2>
+            <h1 className={`w-[330px] h-14 overflow-hidden font-semibold text-lg  ml-3 mt-[215px] ${!theme && "text-slate-700"} ${theme && "text-slate-300"}`}>{snippet.title}</h1>
+            <h2 className={`flex items-center gap-2 text-lg font-bold  ml-3 ${theme && "text-slate-300"}`}> <FaUserCircle className='text-[#FF0000] text-2xl' />{snippet.channelTitle}</h2>
             <h3 className=' ml-3'>{statistics.viewCount} Views</h3>
 
         </div>
